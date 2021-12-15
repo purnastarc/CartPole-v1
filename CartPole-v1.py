27a629790ede6afb7eef3c1cfd2a24a06f379bf3
Purna Chandra Pattipati <67554595+purnastarc@users.noreply.github.com>
2021-12-15 21:52:06 +0530

    Update CartPole-v1.py

--- CartPole-v1.py
+++ CartPole-v1.py
@@ -39,6 +39,7 @@ EPSILON_REDUCE = 0.995
 LEARNING_RATE = 0.001 
 GAMMA = 0.95
 
+# stochastic policy
 def epsilon_greedy_action_selection(model, epsilon, observation):
     if np.random.random() > epsilon:
         prediction = model.predict(observation) 
@@ -50,6 +51,7 @@ def epsilon_greedy_action_selection(model, epsilon, observation):
 replay_buffer = deque(maxlen=20000)
 update_target_model = 10
 
+# Experince replay buffer function
 def replay(replay_buffer, batch_size, model, target_model):
     
     if len(replay_buffer) < batch_size: 
@@ -60,23 +62,18 @@ def replay(replay_buffer, batch_size, model, target_model):
     
     target_batch = []  
     
-    # Efficient way to handle the sample by using the zip functionality
     zipped_samples = list(zip(*samples))  
     states, actions, rewards, new_states, dones = zipped_samples  
     
-    # Predict targets for all states from the sample
     targets = target_model.predict(np.array(states))
     
-    # Predict Q-Values for all new states from the sample
+   
     q_values = model.predict(np.array(new_states))  
     
-    # Now we loop over all predicted values to compute the actual targets
     for i in range(batch_size):  
-        
-        # Take the maximum Q-Value for each sample
+       
         q_value = max(q_values[i][0])  
         
-        # Store the ith target in order to update it according to the formula
         target = targets[i].copy()  
         if dones[i]:
             target[0][actions[i]] = rewards[i]
@@ -84,38 +81,38 @@ def replay(replay_buffer, batch_size, model, target_model):
             target[0][actions[i]] = rewards[i] + q_value * GAMMA
         target_batch.append(target)
 
-    # Fit the model based on the states and the updated targets for 1 epoch
     model.fit(np.array(states), np.array(target_batch), epochs=1, verbose=0)  
 
+ # Copying the weights from Q-network to Target-network
 def update_model_handler(epoch, update_target_model, model, target_model):
     if epoch > 0 and epoch % update_target_model == 0:
         target_model.set_weights(model.get_weights())
 
 model.compile(loss='mse', optimizer=Adam(learning_rate=LEARNING_RATE))
 
-
+# training the Q-network model
 best_so_far = 0
 for epoch in range(EPOCHS):
-    observation = env.reset()  # Get inital state
+    observation = env.reset() 
     
-    # Keras expects the input to be of shape [1, X] thus we have to reshape
+    
     observation = observation.reshape([1, 4])  
     done = False  
     
     points = 0
-    while not done:  # as long current run is active
+    while not done: 
         
         # Select action acc. to strategy
         action = epsilon_greedy_action_selection(model, epsilon, observation)
         
         # Perform action and get next state
         next_observation, reward, done, info = env.step(action)  
-        next_observation = next_observation.reshape([1, 4])  # Reshape!!
-        replay_buffer.append((observation, action, reward, next_observation, done))  # Update the replay buffer
-        observation = next_observation  # update the observation
+        next_observation = next_observation.reshape([1, 4])
+        replay_buffer.append((observation, action, reward, next_observation, done))
+        observation = next_observation 
         points+=1
 
-        # Most important step! Training the model by replaying
+      
         replay(replay_buffer, 32, model, target_model)
 
     
@@ -128,8 +125,12 @@ for epoch in range(EPOCHS):
         best_so_far = points
     if epoch %25 == 0:
         print(f"{epoch}: Points reached: {points} - epsilon: {epsilon} - Best: {best_so_far}")
+        
+
+model.save("mask_detector.model", save_format="h5")
 
 
+# Playing the game
 observation = env.reset()
 for counter in range(300):
     env.render()
