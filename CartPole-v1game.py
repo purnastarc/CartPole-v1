707b01577d07d405a2d192be5b1364b4dbf32630
Purna Chandra Pattipati <67554595+purnastarc@users.noreply.github.com>
2021-12-26 16:49:13 +0530

    Update CartPole-v1game.py

--- CartPole-v1game.py
+++ CartPole-v1game.py
@@ -1,14 +1,8 @@
-from collections import deque
 import random
 import time
-
 import numpy as np
-import gym  # Contains the game we want to play
-from tensorflow.keras.models import Sequential  # To compose multiple Layers
-from tensorflow.keras.layers import Dense  # Fully-Connected layer
-from tensorflow.keras.layers import Activation  # Activation functions
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import clone_model,load_model
+import gym 
+from tensorflow.keras.models import load_model
 
 env_name = 'CartPole-v1'
 env = gym.make(env_name)  # create the environment
@@ -16,21 +10,15 @@ env = gym.make(env_name)  # create the environment
 saved_model=load_model("CartPole-v1_model.h5")
 
 observation = env.reset()
-Score=0
-time.sleep(5)
 
 for counter in range(300):
     env.render()
     time.sleep(0.05)
-    # TODO: Get discretized observation
+   
     action = np.argmax(saved_model.predict(observation.reshape([1,4])))
-    
-    # TODO: Perform the action 
-    observation, reward, done, info = env.step(action) # Finally perform the action
-    Score=Score+1
-    
+  
+    observation, reward, done, info = env.step(action)
+ 
     if done:
-        print("Score: ")
-        print(Score)
         break
 env.close()
